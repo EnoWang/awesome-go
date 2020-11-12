74c185afbdabf70802afc06c5f2446590f080d08
Yinuo Wang <yinuowang0825@gmail.com>
2020-11-11 22:23:08 -0800

    goroutine

--- src/ch8_goroutine/L_07_01_goroutine_test.go
+++ src/ch8_goroutine/L_07_01_goroutine_test.go
@@ -1,1 +1,25 @@
 package ch8_goroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+// 0. goroutine -> Coroutine
+/*
+	Is a lighter thread
+ */
+func TestGoroutine(t *testing.T) {
+
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			for {
+				fmt.Printf("Hello from "+"goroutine %d\n", i)
+			}
+		}(i)
+	}
+
+	// note: make sure main function will not finish suddenly which will kill all goroutine
+	time.Sleep(time.Millisecond)
+}
